d6203579135ab9f9dbd5592467a495a070cd41b8
Michał Albrycht <michalalbrycht@gmail.com>
2019-03-01 00:41:39 +0100

    Fix for require fuzzyset (which is used only in tests)

--- src/moved_block_detector.js
+++ src/moved_block_detector.js
@@ -1,4 +1,8 @@
-const fuzzyset = require('./fuzzyset');
+try {
+    require('./fuzzyset');
+} catch (ex) {
+    // do nothing
+}
 
 class DefaultDict {
   constructor(defaultInit) {
@@ -164,6 +168,7 @@ class MovedBlocksDetector {
             }
             for (const fuzz_pair of fuzzy_matching_pairs) {
                 let [match_probability, text] = fuzz_pair;
+                console.log(`ORYG: ${removed_line.trim_text}\nCOPY: ${text}\nPROB: ${match_probability}\n`);
                 let added_lines = this.trim_text_to_array_of_added_lines[text];
                 for (const added_line of added_lines) {
                     let line_extended_any_block = false;
